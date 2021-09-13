9e5a901a88d79f85a0947fd55bd2d788740a2660
ravindra-me <ravindras@sigmoidanalytics.com>
2021-09-13 23:24:10 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import FormUsingState from "./FormUsingState";
-import FormUsingReducer from "./FormUsingReducer";
+import FormUsingState from './FormUsingState';
+import FormUsingReducer from './FormUsingReducer';
 
 function App() {
   return (
     <>
-      <h2>Form Using State</h2>
+      <h2>Form Using State ()</h2>
       <FormUsingState />
       <h2>Form Using Reducer</h2>
       <FormUsingReducer />
